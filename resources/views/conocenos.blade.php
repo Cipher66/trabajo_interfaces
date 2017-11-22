242b666e016c58f986c6126c49de6c691cf313e3
Cipher66 <schavezfe16dw@ikzubirimanteo.com>
2017-11-22 14:28:17 +0100

    >Carrusel con imágenes

--- resources/views/conocenos.blade.php
+++ resources/views/conocenos.blade.php
@@ -73,6 +73,73 @@
    <!-- <div id="map"></div>
     <div id="map"></div> -->
 </div>
+<div class="container">
+    <br>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+
+            <div class="item active">
+                <img src="images/A400M.jpg" alt="Chania" width="460" height="345">
+                <div class="carousel-caption">
+                    <h3>A400M "Atlas"</h3>
+                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="images/A330_MRTT.jpg" alt="Chania" width="460" height="345">
+                <div class="carousel-caption">
+                    <h3>A330 Multi Role Tanker Transport</h3>
+                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="images/EF2000.jpg" alt="Flower" width="460" height="345">
+                <div class="carousel-caption">
+                    <h3>Eurofighter Typhoon</h3>
+                    <p>Beautiful flowers in Kolymbari, Crete.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="images/EC725.jpg" alt="Flower" width="460" height="345">
+                <div class="carousel-caption">
+                    <h3>EC-725 "Super Cougar"</h3>
+                    <p>Beautiful flowers in Kolymbari, Crete.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="images/NH90.jpg" alt="Flower" width="460" height="345">
+                <div class="carousel-caption">
+                    <h3>NH90</h3>
+                    <p>Beautiful flowers in Kolymbari, Crete.</p>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
 <footer class="section footer">
     <center><h3><b>Contactos de redes sociales</b></h3></center>
     <br />
